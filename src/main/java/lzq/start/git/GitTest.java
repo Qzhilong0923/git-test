4f61df7630c2a52cbfe9eaca11057ec3c5d1b617
longzq <1299166485@qq.com>
2022-05-13 19:54:17 +0800

    first commit

---
+++ src/main/java/lzq/start/git/GitTest.java
@@ -0,0 +1,11 @@
+package lzq.start.git;
+
+/**
+ * @author lzqstart
+ * @create 2022-05-13 19:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
